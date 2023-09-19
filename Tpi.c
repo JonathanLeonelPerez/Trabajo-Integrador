4b30444576db8166ac039ab0002f67cdb112e3c4
JonathanLeonelPerez <145220952+JonathanLeonelPerez@users.noreply.github.com>
2023-09-19 13:02:54 -0300

    Tpi.c

--- Tpi.c
+++ Tpi.c
@@ -30,28 +30,47 @@ int main(){
     }
     //Sintaxis del juego principal        
     //falta hacer que se repita 10 veces, si no adivina las 10 veces tiene que mostrar un fin del juego
-
-
+    //variables para ver que numeros no estan en el arreglo numAleatorio
+    int noRepetidos[5];
+    //contador para mostrar un nuevo arreglo con los numeros que no se repiten en numAleatorio
+    int contador = 0;
 
     
     
     printf("Ingrese un numero: ");
     scanf("%d %d %d %d %d", &numIngresado[0],&numIngresado[1],&numIngresado[2],&numIngresado[3], &numIngresado[4]);
-
+    //Expresion que ve que numeros del arreglo numIngresado no se repiten en numAleatorio
+    for(int i =0; i < 5; i++){
+        int encontrado =0;
+        for(int j =0; j < 5; j++){
+            if(numIngresado[i] == numAleatorio[j]){
+                encontrado = 1;
+                break;
+            }
+        }
+        if(!encontrado){
+            noRepetidos[contador] = numIngresado[i];
+            contador++;
+        }
+    }
+    for(int i = 0;i < contador; i++){
+        printf("El numero %d es incorrecto\n", noRepetidos[i]);
+    }
+    //Termina aca
+    //Aca empieza la expresion que dice que numeros estan correctos y si estan o no en la posicion correcta
     for(int i=0; i < 5; i++){
         for(int j =0; j < 5;j++){
             if(numIngresado[i] == numAleatorio[j]){
-                
                 if(numIngresado[i] == numAleatorio[i]){
                     printf("El numero %d es correcto y la posicion es correcta\n", numIngresado[i]);
                 } else {
-                    printf("El numero %d se repite pero la posicion esta incorrecta\n", numIngresado[i]);
-                }
+                    printf("El numero %d es correcto pero la posicion esta incorrecta\n", numIngresado[i]);
+                } 
                 break;
             }
         }
     }
-    
+    //Esto es para ver nosotros si los numeros estan bien o mal, despues se tiene que borrar!
     for(int i = 0; i < 5; i++){
         printf("%d ", numAleatorio[i]);
     }
@@ -69,13 +88,15 @@ int main(){
     return 0;
 }
 
-
+//Funcion que solicita el nombre del jugadoor
 char solicitarNombre(struct juego *jugador){
     printf("Ingrese su usuario: ");
     fgets(jugador->nombre,sizeof(jugador->nombre), stdin);
 
 }
+//Funcion que saluda al jugador y muestra las instrucciones del juego.
 char saludarJugador(struct juego *jugador){
     printf("Hola %s\n", jugador->nombre);
-    printf("nose que poner(instrucciones)\n");
+    printf("Consignas del juego 'adivina el numero':\n\n El juego es simple, tenes que adivinar un numero aleatorio de 5 digitos.\n Cada intento te marcara un color dependiendo que tan cerca estuviste de adivinar el numero:\n\n VERDE: Numero correcto en la posicion correcta.\n AMARILLO: Numero correcto en la posicion incorrecta.\n ROJO: Numero que no pertenece a los 5 digitos de numero.\n\n Una vez dicho esto, diviertete y A JUGAR!\n");
+
 }
